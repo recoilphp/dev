57c6db7557fe3d864f71fd418746a23d4b7c1c30
James Harris <james.harris@icecave.com.au>
2016-07-27 12:37:32 +1000

    Add tests for `TraceInstaller`.

---
+++ test/suite/Instrumentation/TraceInstaller.spec.php
@@ -0,0 +1,88 @@
+<?php
+
+declare (strict_types = 1); // @codeCoverageIgnore
+
+namespace Recoil\Dev\Instrumentation;
+
+use Eloquent\Phony\Phony;
+use Recoil\Kernel\StrandTrace;
+use Recoil\Recoil;
+
+describe(TraceInstaller::class, function () {
+
+    if (ini_get('zend.assertions') > 0) {
+        it('installs an instrumentation trace', function () {
+            yield new TraceInstaller();
+            $strand = yield Recoil::strand();
+
+            expect($strand->trace())->to->be->an->instanceof(Trace::class);
+        });
+
+        it('does not replace an existing trace', function () {
+            $trace = Phony::mock(StrandTrace::class)->get();
+            $strand = yield Recoil::strand();
+            $strand->setTrace($trace);
+
+            yield new TraceInstaller();
+
+            expect($strand->trace())->to->equal($trace);
+        });
+
+        it('returns a newly installed instrumentation trace', function () {
+            $trace = yield new TraceInstaller();
+            $strand = yield Recoil::strand();
+
+            expect($strand->trace())->to->equal($trace);
+        });
+
+        it('returns a previously installed instrumentation trace', function () {
+            $trace = yield new TraceInstaller();
+
+            expect(yield new TraceInstaller())->to->equal($trace);
+        });
+
+        it('does not replace an existing third-party trace', function () {
+            $trace = Phony::mock(StrandTrace::class)->get();
+            $strand = yield Recoil::strand();
+            $strand->setTrace($trace);
+
+            yield new TraceInstaller();
+
+            expect($strand->trace())->to->equal($trace);
+        });
+
+        it('returns null when there is an existing third-party trace', function () {
+            $trace = Phony::mock(StrandTrace::class)->get();
+            $strand = yield Recoil::strand();
+            $strand->setTrace($trace);
+
+            expect(yield new TraceInstaller())->to->be->null;
+        });
+    }
+
+});
+// use Recoil\Kernel\Api;
+// use Recoil\Kernel\Awaitable;
+// use Recoil\Kernel\Listener;
+// use Recoil\Kernel\Strand;
+//
+// final class TraceInstaller implements Awaitable
+// {
+//     public function await(Listener $listener, Api $api)
+//     {
+//         assert($listener instanceof Strand);
+//
+//         $trace = $listener->trace();
+//
+//         if ($trace === null) {
+//             $trace = new Trace();
+//             $listener->setTrace($trace);
+//         }
+//
+//         if ($trace instanceof Trace) {
+//             $listener->send($trace);
+//         } else {
+//             $listener->send(null);
+//         }
+//     }
+// }
