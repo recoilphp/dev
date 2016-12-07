5badbd001adccc19eb5b4c34b1e4ace26453aa78
James Harris <james.harris@icecave.com.au>
2016-12-07 11:00:03 +1000

    Add function test for sleep/wake ordering.

--- test-kernel/api/functional.sleep.spec.php
+++ test-kernel/api/functional.sleep.spec.php
@@ -6,11 +6,12 @@ namespace Recoil;
 
 context('api/sleep', function () {
     rit('resumes execution after the specified number of seconds', function () {
+        $operation = Recoil::sleep(0.02);
         $time = microtime(true);
-        yield Recoil::sleep(0.02);
+        yield $operation;
         $diff = microtime(true) - $time;
 
-        expect($diff)->to->be->within(0.01, 1.03);
+        expect($diff)->to->be->within(0.01, 0.03);
     });
 
     rit('can be invoked by yielding a number', function () {
@@ -34,5 +35,22 @@ context('api/sleep', function () {
         $diff = microtime(true) - $time;
 
         expect($diff)->to->be->below(0.01);
+    });
+
+    rit('wakes strands in the correct order', function () {
+        $strand1 = yield Recoil::execute(function () {
+            yield 0.02;
+            echo 'b';
+        });
+
+        $strand2 = yield Recoil::execute(function () {
+            yield 0.01;
+            echo 'a';
+        });
+
+        ob_start();
+        yield $strand1;
+        yield $strand2;
+        expect(ob_get_clean())->to->equal('ab');
     });
 });
