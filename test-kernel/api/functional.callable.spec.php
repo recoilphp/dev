c7b20ccfac304d6f38f03f74408105df55a52767
James Harris <james.harris@icecave.com.au>
2016-12-13 13:57:28 +1000

    Add functional tests for callables as dispatchable values (recoilphp/recoil#99).

---
+++ test-kernel/api/functional.callable.spec.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1); // @codeCoverageIgnore
+
+namespace Recoil;
+
+use Exception;
+use UnexpectedValueException;
+
+context('api/callable', function () {
+    context('when it returns a generator', function () {
+        rit('invokes the generator as a coroutine', function () {
+            $fn = function () {
+                return '<result>';
+                yield;
+            };
+
+            expect(yield $fn)->to->equal('<result>');
+        });
+    });
+
+    context('when it returns a non-generator', function () {
+        rit('resumes the calling strand with an exception', function () {
+            $fn = function () {
+                return '<string>';
+            };
+
+            try {
+                yield $fn;
+                expect(false)->to->be->ok('expected exception was not thrown');
+            } catch (UnexpectedValueException $e) {
+                // ok
+            }
+        });
+    });
+});
