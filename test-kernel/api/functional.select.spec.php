84d30b4f1136e02daa205eb689063d562d5c0b6f
James Harris <james.harris@icecave.com.au>
2017-01-09 11:20:31 +1000

    Add functional tests for `select()`.

---
+++ test-kernel/api/functional.select.spec.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1); // @codeCoverageIgnore
+
+namespace Recoil;
+
+context('api/select', function () {
+    beforeEach(function () {
+        $this->streams = [];
+        $this->files = [];
+
+        for ($i = 0; $i < 2; ++$i) {
+            $temp = tempnam(sys_get_temp_dir(), 'recoil-test-fifo-');
+            unlink($temp);
+            posix_mkfifo($temp, 0644);
+            $stream = fopen($temp, 'w+'); // must be w+ (read/write) to prevent blocking
+            stream_set_blocking($stream, false);
+
+            $this->streams[] = $stream;
+            $this->files[] = $temp;
+        }
+
+        list($this->stream1, $this->stream2) = $this->streams;
+    });
+
+    afterEach(function () {
+        foreach ($this->streams as $stream) {
+            fclose($stream);
+        }
+        foreach ($this->files as $file) {
+            unlink($file);
+        }
+    });
+
+    rit('returns an array of streams that are ready to read', function () {
+        fwrite($this->stream2, '<value>');
+
+        list($readable) = yield Recoil::select($this->streams, []);
+
+        expect($readable)->to->equal([$this->stream2]);
+    });
+
+    rit('returns an array of streams that are ready to write', function () {
+        // fill the write buffer
+        do {
+            $bytes = fwrite(
+                $this->stream1,
+                str_repeat('.', 8192)
+            );
+        } while ($bytes > 0);
+
+        list(, $writable) = yield Recoil::select([], $this->streams);
+
+        expect($writable)->to->equal([$this->stream2]);
+    });
+
+    rit('returns arrays of streams that are ready to read and write', function () {
+        fwrite($this->stream1, '<value>');
+
+        // fill the write buffer
+        do {
+            $bytes = fwrite(
+                $this->stream1,
+                str_repeat('.', 8192)
+            );
+        } while ($bytes > 0);
+
+        list($readable, $writable) = yield Recoil::select($this->streams, $this->streams);
+
+        expect($readable)->to->equal([$this->stream1]);
+        expect($writable)->to->equal([$this->stream2]);
+    });
+
+    rit('returns stream in both arrays when ready for both reading and writing', function () {
+        fwrite($this->stream1, '<value>');
+
+        // fill the write buffer
+        do {
+            $bytes = fwrite(
+                $this->stream2,
+                str_repeat('.', 8192)
+            );
+        } while ($bytes > 0);
+
+        list($readable, $writable) = yield Recoil::select($this->streams, $this->streams);
+
+        expect($readable)->to->equal($this->streams);
+        expect($writable)->to->equal([$this->stream1]);
+    });
+
+    rit('stops waiting for the stream when the strand is terminated', function () {
+        $strand = yield Recoil::execute(function () {
+            yield Recoil::select([$this->stream1], []);
+            expect(false)->to->be->ok('strand was not terminated');
+        });
+
+        yield;
+
+        $strand->terminate();
+    });
+});
